b6a71e971b6c3a4ba4905e72252e314ef1dd624f
kanes115 <dominikstanaszek@icloud.com>
2017-03-13 19:11:19 +0100

    dunamic linking test

---
+++ TestProg/test_dynamic.c
@@ -0,0 +1,38 @@
+#include "basetoTest.c"
+#include "../listAddBook.h"
+#include "../bintreeAddBook.h"
+#include <dlfcn.h>
+
+typedef struct functions{
+
+}funcs;
+
+
+
+int main(int argc, char* argv []){
+
+  void* handle;
+
+  handle = dlopen("../libAddressBook.so", RTLD_LAZY);
+
+  if (!handle){
+    fprintf(stderr, "%s\n", dlerror());
+    return 1;
+  }
+
+  Book_bin* (*createEmptyAddBook_bin)(ORGANIZED_BY);
+
+  createEmptyAddBook_bin = dlsym(handle, "createEmptyAddBook_bin");
+
+  error = dlerror();
+   if (error != NULL) {
+     fprintf(stderr, "%s\n", error);
+     return 1;
+    }
+
+  Book_bin* res = (*createEmptyAddBook_bin)(0);
+
+  dlclose(handle);
+
+
+}
